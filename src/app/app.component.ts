4367763386a2265cad9b377f8dfce15caa083049
Hope Ngerebara <hope.ngerebara@andela.com>
2017-09-28 13:43:30 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-root',
+    template: '<h2>Hello world!</h2>',
+    styles: [
+        `
+        h2 {
+            font-family: sans-sarif,
+            font-size: 1.2em
+        }
+        `
+    ]
+})
+export class AppComponent {
+
+}
