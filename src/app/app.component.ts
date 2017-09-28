0034d82494eb175f31db63acb03dd1a003798226
Hope Ngerebara <hope.ngerebara@andela.com>
2017-09-28 14:01:51 +0100

    seperate css and html from component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'app-root',
-    template: '<h2>Hello world!</h2>',
-    styles: [
-        `
-        h2 {
-            font-family: sans-sarif,
-            font-size: 1.2em
-        }
-        `
-    ]
+    template: 'app.component.html',
+    styles: ['app.component.css']
 })
 export class AppComponent {
 
